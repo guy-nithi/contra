1e052396e310bbaf6a7458342540ca2fcc4d43a5
Nithikrit Raibroycharoen <greninja028@Nithikrits-MacBook-Air.local>
2022-01-07 14:03:55 +0700

    commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-import time
+
 
 tiles = 11
 tile_size = 64
@@ -12,22 +12,34 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 
 running = True
 
-idle_char = pygame.image.load('/Users/greninja028/Desktop/Coding/contra/1.png').convert_alpha()
-idle_char_pos_x, idle_char_pos_y = screen_width//2, screen_height//2
+idle_char = pygame.image.load('/Users/greninja028/Desktop/Coding/contra/BillRizerRuns.png').convert_alpha()
+# idle_char = pygame.transform.scale(screen, (1,1))
+idle_char_pos_x, idle_char_pos_y = screen_width//2, 0
 
-gravity = 1
+GRAVITY = 1
 
 clock = pygame.time.Clock()
+rect_platform = pygame.Rect(0, 234, 1200, 25)
 
 def load_images():
     global idle_char
     screen.blit(idle_char, (idle_char_pos_x, idle_char_pos_y))
 
+# def test():
+
+def check_collision():
+    global rect_platform
+    pygame.draw.rect(screen, (255, 255, 255), rect_platform)
+    rect = idle_char.get_rect()
+    player_collided = pygame.Rect.colliderect(rect, rect_platform)
+    if player_collided:
+        rect.bottom = rect_platform.top
+
 def main():
 
     FPS = 60
 
-    global running, idle_char, idle_char_pos_x, idle_char_pos_y
+    global running, idle_char, idle_char_pos_x, idle_char_pos_y, rect_platform
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -35,11 +47,14 @@ def main():
                 pygame.quit()
                 sys.exit(0)
             screen.fill('black')
-            idle_char_pos_y += gravity
             load_images()
+            # test()
+        check_collision()
 
-            # pygame.display.update()
+        idle_char_pos_y += GRAVITY
+
         keys = pygame.key.get_pressed()
+
 
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             idle_char_pos_x += 5
